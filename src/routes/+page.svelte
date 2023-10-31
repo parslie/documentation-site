f3a677b2efa775cba7c1ff2ab2c5ee26c3edaf0f
Viktor Holta <viktor.holta@outlook.com>
2023-10-31 17:35:44 +0100

    NEW: init project with static file test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let text = "";
+    
+    onMount(() => {
+        loadText();
+    });
+
+    async function loadText() {
+        const resp = await fetch("test.md");
+        text = await resp.text();
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{text}</p>
